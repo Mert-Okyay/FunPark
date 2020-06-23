357cd570e71d2a26722e17dfe4f01d43c1c76248
Mert-Okyay <mxo431@miami.edu>
2020-06-23 19:29:23 -0400

    FunPArk

---
+++ Assets/MouseLook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float MouseX;
+    float MouseY;
+    public float MouseSensitivity = 100f;
+    public Transform Character;
+
+    float xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetMousePosition();
+        RotateCharacterWithMouse();
+    }
+
+    private void RotateCharacterWithMouse()
+    {
+        xRotation -= MouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        Character.Rotate(Vector3.up * MouseX);
+    }
+
+    private void GetMousePosition()
+    {
+        MouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        MouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float x;
+    float z;
+    public float speed = 12f;
+
+    Boolean gravityOn;
+    public CharacterController controller;
+    public Rigidbody characterBody;
+    void Start()
+    {
+        gravityOn = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        /*if (Input.GetKeyDown("space"))
+        {
+            if(gravityOn){
+                characterBody.useGravity = false;
+            }
+            else
+            {
+                characterBody.useGravity = true;
+            }
+            
+        }*/
+
+        if(Input.GetKeyDown("h")){
+            transform.localPosition = new Vector3(0f,0.39f,-166.7f);
+        }
+
+        if (Input.GetKeyDown("x"))
+        {
+            Application.Quit();
+        }
+    }
+}
